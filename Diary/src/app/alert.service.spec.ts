e4156397d9b208439a87920402c328ac55dbfd48
kahenya-anita <anitakahenya1@gmail.com>
2020-08-20 17:37:20 +0300

    Add classes

---
+++ Diary/src/app/alert.service.spec.ts
@@ -0,0 +1,7 @@
+import { Alert.Service } from './alert.service';
+
+describe('Alert.Service', () => {
+  it('should create an instance', () => {
+    expect(new Alert.Service()).toBeTruthy();
+  });
+});
--- Diary/src/app/app.module.ts
+++ Diary/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Alert } from './alert.service/alert.service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Alert.ServiceComponent
   ],
   imports: [
     BrowserModule,
